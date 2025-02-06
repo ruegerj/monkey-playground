644468076d08573a8e301e40aa6b6a1ce19d4c7c
ruegerj <jan.rueger@gmx.ch>
2025-02-06 11:01:39 +0100

    add oauth flow for github

---
+++ src/routes/login/github/+server.ts
@@ -0,0 +1,22 @@
+import { github } from '$lib/server/oauth';
+import type { RequestEvent } from '@sveltejs/kit';
+import { generateState } from 'arctic';
+
+export async function GET(event: RequestEvent): Promise<Response> {
+	const state = generateState();
+	const url = github.createAuthorizationURL(state, []);
+
+	event.cookies.set('github_oauth_state', state, {
+		path: '/',
+		httpOnly: true,
+		maxAge: 60 * 10,
+		sameSite: 'lax'
+	});
+
+	return new Response(null, {
+		status: 302,
+		headers: {
+			Location: url.toString()
+		}
+	});
+}
---
+++ src/routes/login/github/callback/+server.ts
@@ -0,0 +1,66 @@
+import { createSession, generateSessionToken, setSessionTokenCookie } from '$lib/server/auth';
+import { github } from '$lib/server/oauth';
+import { createUser, getUserByGithubId, type User } from '$lib/server/router/user';
+import type { RequestEvent } from '@sveltejs/kit';
+import type { OAuth2Tokens } from 'arctic';
+
+export async function GET(event: RequestEvent): Promise<Response> {
+	const code = event.url.searchParams.get('code');
+	const state = event.url.searchParams.get('state');
+	const storedState = event.cookies.get('github_oauth_state') ?? null;
+	if (code === null || state === null || storedState === null) {
+		return new Response(null, {
+			status: 400
+		});
+	}
+	if (state !== storedState) {
+		return new Response(null, {
+			status: 400
+		});
+	}
+
+	let tokens: OAuth2Tokens;
+	try {
+		tokens = await github.validateAuthorizationCode(code);
+	} catch (e) {
+		console.warn('Failed to authenticate via GitHub: ', e);
+		// invalid code or clients credentials
+		return new Response(null, {
+			status: 400
+		});
+	}
+
+	const githubUserResponse = await fetch('https://api.github.com/user', {
+		headers: {
+			Authorization: `Bearer ${tokens.accessToken()}`
+		}
+	});
+	const githubUser = await githubUserResponse.json();
+	const githubUserId = githubUser.id;
+	const githubUsername = githubUser.login;
+
+	const existingUser = await getUserByGithubId(githubUserId);
+	if (existingUser) {
+		console.log(`Found existing user: ${existingUser.id}, proceeding to authenticate...`);
+		return authenticate(event, existingUser);
+	}
+
+	const createdUser = await createUser(githubUserId, githubUsername);
+	console.log(`Created user with name: ${githubUsername}`);
+	return authenticate(event, createdUser);
+}
+
+async function authenticate(event: RequestEvent, user: User): Promise<Response> {
+	const sessionToken = generateSessionToken();
+	const session = await createSession(sessionToken, user.id);
+	setSessionTokenCookie(event, sessionToken, session.expiresAt);
+
+	console.log(`Authenticated user: ${user.id} until: ${session.expiresAt}`);
+
+	return new Response(null, {
+		status: 302,
+		headers: {
+			Location: '/'
+		}
+	});
+}
