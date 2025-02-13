8d84d861d35d2f255fd24d489d4d245809cd3f69
ruegerj <jan.rueger@gmx.ch>
2025-02-13 18:30:33 +0100

    add toasts
    for displaying notifications to the user.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
+	import { Toaster } from '$lib/components/ui/sonner';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
 <ModeWatcher />
+<Toaster />
+
 {@render children()}
