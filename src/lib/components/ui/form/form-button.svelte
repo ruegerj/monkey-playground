66c7500155975d1998087660a76eed720efcf03e
ruegerj <jan.rueger@gmx.ch>
2025-02-21 10:34:28 +0100

    add form components

---
+++ src/lib/components/ui/form/form-button.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import * as Button from '$lib/components/ui/button/index.js';
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	type $$Props = Button.Props;
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	type $$Events = Button.Events;
+</script>
+
+<Button.Root type="submit" on:click on:keydown {...$$restProps}>
+	<slot />
+</Button.Root>
---
+++ src/lib/components/ui/form/form-description.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import * as FormPrimitive from 'formsnap';
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { cn } from '$lib/utils.js';
+	import type { ClassValue } from 'clsx';
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	type $$Props = HTMLAttributes<HTMLSpanElement>;
+	let className: ClassValue | undefined | null = undefined;
+	export { className as class };
+</script>
+
+<FormPrimitive.Description
+	class={cn('text-sm text-muted-foreground', className)}
+	{...$$restProps}
+	let:descriptionAttrs
+>
+	<slot {descriptionAttrs} />
+</FormPrimitive.Description>
