9c6c3cf20d0ce7441f5f1d91f25ee4589f6e6944
ruegerj <jan.rueger@gmx.ch>
2025-02-24 21:30:00 +0100

    add relative timestamp for last edit timestamp

--- src/lib/components/custom/sidebar.svelte
+++ src/lib/components/custom/sidebar.svelte
@@ -4,7 +4,7 @@
 	import IconPlus from 'virtual:icons/lucide/plus';
 	import type { Snippet as SvelteSnippet } from 'svelte';
 	import type { Snippet } from '$lib/server/router/snippet';
-	import { formatDate } from '$lib/date';
+	import { relativeTime } from '$lib/date';
 
 	interface Props {
 		children: SvelteSnippet<[]>;
@@ -27,10 +27,11 @@
 		<Sheet.Header>
 			<Sheet.Title>My snippets</Sheet.Title>
 		</Sheet.Header>
-		<div class="flex-grow overflow-scroll">
+		<div class="flex-grow overflow-y-scroll">
 			{#each snippets as snippet}
-				<Button variant="link" class="block" href={'/' + snippet.id} onclick={close}>
+				<Button variant="link" class="block break-all" href={'/' + snippet.id} onclick={close}>
 					{snippet.name}
+					<i class="text-muted-foreground">{relativeTime(snippet.updatedAt)}</i>
 				</Button>
 			{/each}
 		</div>
